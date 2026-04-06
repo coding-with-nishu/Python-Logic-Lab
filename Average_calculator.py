99962b82a5260171a2dce30d7a67899e9c734d2e
Nishu <nishukararwal101@gmail.com>
2026-04-06 22:41:56 +0530

    Update Average_calculator.py

--- Average_calculator.py
+++ Average_calculator.py
@@ -2,5 +2,5 @@
 sub1=float(input("enter the marks:-"))
 sub2=float(input("enter the marks:-"))
 sub3=float(input("enter the marks:-"))
-Average=(sub1+sub2+sub3)/2
+Average=(sub1+sub2+sub3)/3
 print(Average)
